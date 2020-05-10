a3cca60a62e27e4f04d3a8df92f48ed1833e65c6
Julio Cavalin <julio.cavalin@gmail.com>
2020-05-10 19:02:46 -0300

    Updates

---
+++ precache-manifest.93550aa44fdfb64894220aba2c803905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098c810f0bf9fc2345406aaf16748b88",
+    "url": "/nomes-alternativos/index.html"
+  },
+  {
+    "revision": "ab93ff8252a77905511b",
+    "url": "/nomes-alternativos/static/css/main.c6a08e86.chunk.css"
+  },
+  {
+    "revision": "2ac088755a2bf915a04f",
+    "url": "/nomes-alternativos/static/js/2.417450bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nomes-alternativos/static/js/2.417450bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab93ff8252a77905511b",
+    "url": "/nomes-alternativos/static/js/main.e3932f8a.chunk.js"
+  },
+  {
+    "revision": "9490b3a813609e81c072",
+    "url": "/nomes-alternativos/static/js/runtime-main.2b50c5e3.js"
+  }
+]);
